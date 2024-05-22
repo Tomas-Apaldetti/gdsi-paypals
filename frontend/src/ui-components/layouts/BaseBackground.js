bc43cae4fe360189692784cbf66b4432f7ac7c34
Lucia Napoli <lucianapoli15@gmail.com>
2024-05-22 15:08:19 -0300

    edit logo

--- frontend/src/ui-components/layouts/BaseBackground.js
+++ frontend/src/ui-components/layouts/BaseBackground.js
@@ -3,7 +3,7 @@ import React from 'react'
 const BaseBackground = ({children}) => {
   return (
     <div className='min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-slate-100 to-slate-400'>
-      <img src='logo.png' width="200" style={{ marginBottom: '30px', marginTop: '20px' }} alt="PayPals"></img>      
+      <img src='logoNegro.png' width="200" style={{ marginBottom: '30px', marginTop: '20px' }} alt="PayPals"></img>      
       {children}
     </div>
   )
--- frontend/src/ui-components/layouts/Navbar/Logo.js
+++ frontend/src/ui-components/layouts/Navbar/Logo.js
@@ -1,10 +1,10 @@
-import { BanknotesIcon } from '@heroicons/react/20/solid';
+// import { BanknotesIcon } from '@heroicons/react/20/solid';
 import React from 'react';
 
 const Logo = () => {
   return (
     <div className='flex items-center'>
-      <img src='logoSlate.png' width="130" alt="PayPals"></img>
+      <img src='logoBlanco.png' width="130" alt="PayPals"></img>
       {/* <BanknotesIcon className='h-8 w-8 mr-2 text-purple-500' />
         <p className='text-purple-500 text-xl font-bold tracking-wider'>PayPals</p> */}
     </div>
