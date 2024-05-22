07c59ec7f524c4df63a39c0ee24c924e7622a5b8
Tomás Apaldetti <tapaldetti@accela.com>
2024-05-22 20:41:47 -0300

    react way

--- frontend/src/ui-components/layouts/BaseBackground.js
+++ frontend/src/ui-components/layouts/BaseBackground.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import logoNegro from 'public/logoNegro.png'
 
 const BaseBackground = ({children}) => {
   return (
     <div className='min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-slate-100 to-slate-400'>
-      <img src='logoNegro.png' width="200" style={{ marginBottom: '30px', marginTop: '20px' }} alt="PayPals"></img>      
+      <img src={logoNegro} width="200" style={{ marginBottom: '30px', marginTop: '20px' }} alt="PayPals"></img>
       {children}
     </div>
   )
