18cf13eb3e9424a2d173b2648b65f19e41038538
Federico Jaleh <39125708+Ezjafe@users.noreply.github.com>
2024-05-28 23:14:04 -0300

    searching by ticket name

--- frontend/src/pages/Ticket/List.js
+++ frontend/src/pages/Ticket/List.js
@@ -4,10 +4,11 @@ import { getTickets } from 'services/tickets';
 import { useSearchParams } from 'react-router-dom';
 import { useAPIData } from 'hooks/useAPIData';
 import { Loading } from 'logic-components/Loading';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function List() {
   const [queryparams] = useSearchParams();
+  const [searchText, setSearchText] = useState('')
 
   const _getTickets = () => {
     return async () => {
@@ -17,15 +18,19 @@ export default function List() {
 
   const { data: tickets, loading, error, setStale } = useAPIData(_getTickets(), [], []);
 
+  const searchTicket = (e) => {
+    e.preventDefault()
+    setSearchText(e.target.value)
+  }
   useEffect(() => {
     setStale(true)
   }, [queryparams, setStale])
 
   return (
     <div className='w-full h-full'>
-      <TopBar onCreation={() => setStale(true)} />
+      <TopBar onCreation={() => setStale(true)} searchTicket={searchTicket}/>
       <Loading loading={loading} error={error}>
-        {tickets.map((ticket, index) => (
+        {tickets.filter((x) => x.name.toLowerCase().includes(searchText)).map((ticket, index) => (
           <Ticket ticket={ticket} key={index} onEdition={() => setStale(true)} />
         ))}
       </Loading>
--- frontend/src/pages/Ticket/TopBar.js
+++ frontend/src/pages/Ticket/TopBar.js
@@ -5,12 +5,12 @@ import { MagnifyingGlassIcon, PlusIcon } from '@heroicons/react/20/solid';
 import { Modal } from 'ui-components/modal/Modal';
 import { TicketCreation } from './Creation';
 
-export const TopBar = ({ onCreation }) => {
+export const TopBar = ({ onCreation, searchTicket }) => {
   const [showCreate, setShowCreate] = useState(false);
 
   return (
     <div className='flex bg-slate-50 px-4 w-full items-center border-b border-slate-200 shadow-sm'>
-      <Input icon={<MagnifyingGlassIcon />} id='search' placeholder='Search' className='h-10' />
+      <Input icon={<MagnifyingGlassIcon />} id='search' placeholder='Search' className='h-10' onChange={searchTicket} />
       <Button className='ml-4 w-12 md:w-28' onClick={() => setShowCreate(true)}>
         <PlusIcon className='h-6 w-6' />
       </Button>
